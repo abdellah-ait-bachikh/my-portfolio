201a849a63a31e8bd66327e50c1e614da524bf3a
abdo261 <abdellah.bs2018@gmail.com>
2025-04-18 11:54:47 +0100

    create & use navigation-context edited

--- src/app/_pages/About.tsx
+++ src/app/_pages/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='h-screen conatiner flex items-center mt-1 backdrop-blur-sm ' id='about'>
+    <div className='h-screen conatiner flex items-center mt-1 ' id='about'>
        About
     </div>
   )
